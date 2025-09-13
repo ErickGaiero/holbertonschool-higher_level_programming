f72a853364ba40596c4ff2f430eaedf4cb4b748b
ErickGaiero <erickgaiero15@gmail.com>
2025-09-13 01:55:31 -0300

    Exercise 8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        first = None
+    if length > 0:
+        first = sentence[0]
+    tuple_result = (length, first)
+    return (tuple_result)
