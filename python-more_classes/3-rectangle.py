537ea66f336fc3bccc8a6fb6bc27c9d23eed5719
ErickGaiero <erickgaiero15@gmail.com>
2025-09-21 23:57:04 -0300

    Exercise 3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -46,7 +46,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-        
+
     def __str__(self):
         """Print the rectangle with the character #"""
         if self.__width == 0 or self.__height == 0:
