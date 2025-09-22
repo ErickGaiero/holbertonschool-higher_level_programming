ddfe25442715aed25a79e74ba50947c10443a902
ErickGaiero <erickgaiero15@gmail.com>
2025-09-22 01:47:45 -0300

    Exercise 9

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -84,6 +84,7 @@ class Rectangle:
             return rect_1
         return rect_2
 
+    @classmethod
     def square(cls, size=0):
         """Return a new Rectangle instance"""
         return cls(size, size)
