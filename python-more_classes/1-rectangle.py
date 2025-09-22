2a0aef89d4ccd5370eeefeab8d0bb847f1807d1a
ErickGaiero <erickgaiero15@gmail.com>
2025-09-21 23:19:07 -0300

    Exercise 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,17 +8,17 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        
+
     @property
     def height(self):
         """Get height of rectangle"""
         return self.__height
-        
+
     @property
     def width(self):
         """Get width of rectangle"""
         return self.__width
-    
+
     @height.setter
     def height(self, height):
         """Set height of rectangle"""
@@ -27,7 +27,7 @@ class Rectangle:
         if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
-        
+
     @width.setter
     def width(self, width):
         """"Set width of rectangle"""""
@@ -36,4 +36,3 @@ class Rectangle:
         if width < 0:
             raise ValueError("width must be >= 0")
         self.__width = width
-        
