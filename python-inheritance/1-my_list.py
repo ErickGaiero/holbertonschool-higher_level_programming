3c8e116d2dfd3bf987dd4cc7231c75f7b22067b9
ErickGaiero <erickgaiero15@gmail.com>
2025-09-23 13:50:00 -0300

    Exercise 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,12 +2,9 @@
 
 """Defines a class MyList that inherits from list."""
 
+
 class MyList(list):
     """Class MyList that inherits from list."""
-
     def print_sorted(self):
         """Prints the list in ascending order."""
         print(sorted(self))
-        
-    def __str__(self):
-        return str(list(self))
