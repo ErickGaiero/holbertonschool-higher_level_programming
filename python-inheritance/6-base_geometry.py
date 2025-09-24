4012ad006c729421939da8e86250d28b9e08d0e3
ErickGaiero <erickgaiero15@gmail.com>
2025-09-24 11:00:41 -0300

    Exercise 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines a base geometry class"""
+
+
+class BaseGeometry:
+    """Base geometry"""
+
+    def area(self):
+        """Raises an exception with a message"""
+        raise Exception("area() is not implemented")
