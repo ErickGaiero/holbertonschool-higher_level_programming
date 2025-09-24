c4f832c84ee488b54b4179d59bdec757224d90fd
ErickGaiero <erickgaiero15@gmail.com>
2025-09-24 10:53:00 -0300

    Exercise 1

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Returns True if the object is an instance of a class that inherited"""
+
+def inherits_from(obj, a_class):
+    """Returns true or false"""
+    return isinstance(obj, a_class) and type(obj) is not a_class
