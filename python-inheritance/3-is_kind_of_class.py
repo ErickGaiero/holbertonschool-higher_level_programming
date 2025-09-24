81bba5ce8f10cd683b21a517cce456e9242ef8ac
ErickGaiero <erickgaiero15@gmail.com>
2025-09-24 09:50:58 -0300

    Exercise 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Returns True if the object is an instance of, or if the object is an"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Instance of a class"""
+    return isinstance(obj, a_class)
