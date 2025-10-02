4f4ace409296283a9cb0bbed66bea4239e0957c7
ErickGaiero <erickgaiero15@gmail.com>
2025-10-02 16:33:47 -0300

    Exercise 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+
+"""Exercises"""
+
+
+def to_json_string(my_obj):
+    """Write a function that returns the JSON representation of an object"""
+    return json.dumps(my_obj)
