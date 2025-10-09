3562b26a45c07c55006674b0c6011946e03c5bb0
ErickGaiero <erickgaiero15@gmail.com>
2025-10-08 22:39:57 -0300

    Exercise 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Exercise 0"""
+
+
+def safe_print_list(my_list=[], x=0):
+    
+    """Write a function that prints x elements of a list."""
+    
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
+    print()
+    return count
