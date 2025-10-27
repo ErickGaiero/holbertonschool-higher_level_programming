dfee1a933d3eafc437fbff038b8b669e8077a73b
ErickGaiero <erickgaiero15@gmail.com>
2025-10-26 22:20:54 -0300

    Exercise 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists privilegies of two users
-SHOW GRANTS FOR 'user_0d1'@'localhost';
-SHOW GRANTS FOR 'user_0d2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
