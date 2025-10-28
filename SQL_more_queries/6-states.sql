2d9ec1b5b6a133ad510a9ef1a10d22211be80166
ErickGaiero <erickgaiero15@gmail.com>
2025-10-28 11:23:32 -0300

    Exercise 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_6 and the states table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_6;
-USE hbtn_0d_6;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
     id INT UNIQUE AUTO_GENERATED NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL
