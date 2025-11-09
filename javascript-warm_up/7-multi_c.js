d09f7caaf4d701e5d3ddf3e3a58c41dab478e240
ErickGaiero <erickgaiero15@gmail.com>
2025-11-09 20:18:03 -0300

    Exercise 7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const x = Number(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let out = '';
+  for (let i = 0; i < x; i++) {
+    if (i === 0) {
+      out = 'C is fun';
+    } else {
+      out = out + '\n' + 'C is fun';
+    }
+  }
+  console.log(out);
+}
