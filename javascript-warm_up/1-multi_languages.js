abf5243410f39a888a65a6c0c01850a102748d78
ErickGaiero <erickgaiero15@gmail.com>
2025-11-09 16:27:15 -0300

    Exercise 1

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -3,6 +3,6 @@ const myVar = 'JavaScript is amazing';
 const myVar2 = 'Python is cool';
 const myVar3 = 'C is fun';
 
-console.log(myVar);
-console.log(myVar2);
 console.log(myVar3);
+console.log(myVar2);
+console.log(myVar);
