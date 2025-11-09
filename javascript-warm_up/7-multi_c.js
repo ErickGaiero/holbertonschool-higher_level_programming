01dad838039f884d9ea5dd15270cde2ea265bdcc
ErickGaiero <erickgaiero15@gmail.com>
2025-11-09 20:46:51 -0300

    Exercise 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const x = Number(process.argv[2]);
 
-if (isNaN(x) || x < 1) {
+if (process.argv[2] === undefined || isNaN(x)) {
   console.log('Missing number of occurrences');
-} else {
+} else if (x > 0) {
   let out = '';
   for (let i = 0; i < x; i++) {
     if (i === 0) {
