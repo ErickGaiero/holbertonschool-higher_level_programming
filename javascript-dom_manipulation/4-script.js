80ac8dc19e748635485595a2d2b42c0188b0b7cb
ErickGaiero <erickgaiero15@gmail.com>
2025-11-12 14:30:06 -0300

    Exercise 4

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('#add_item');
+button.addEventListener('click', function () {
+  const list = document.querySelector('ul.my_list');
+  const li = document.createElement('li');
+  list.appendChild(li);
+  li.innerText = 'Pepe';
+});
