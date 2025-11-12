df1b9c44515d70a49b544f5fee3ce5cae12fed2c
ErickGaiero <erickgaiero15@gmail.com>
2025-11-12 13:28:33 -0300

    Exercise 1

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 //Script that updates the text color of the header element to red (#FF0000) when the user clicks on the tag with id red_header
 document.addEventListener('DOMContentLoaded', function() {
-    const header = document.querySelector('red_header');
+    const redHeader = document.querySelector('#red_header');
+    const header = document.querySelector('header');
+    
+    redHeader.addEventListener('click', function(){
     header.style.color = '#FF0000';
+    });
 });
